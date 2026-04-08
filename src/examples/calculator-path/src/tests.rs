b50a0b0d9b8435ef5f0db90bf7619f4e07685177
Gabor Szabo <gabor@szabgab.com>
2026-04-08 09:07:24 +0200

    rename tests; test for 404 page

--- src/examples/calculator-path/src/tests.rs
+++ src/examples/calculator-path/src/tests.rs
@@ -10,52 +10,91 @@ async fn test_main_page() {
 }
 
 #[tokio::test]
-async fn test_add_v1() {
+async fn test_v1_add() {
     check_equals("/v1/add/2/3", "2 + 3 = 5").await;
     check_equals("/v1/add/7/8", "7 + 8 = 15").await;
 }
 
 #[tokio::test]
-async fn test_subtraction_v1() {
+async fn test_v1_subtraction() {
     check_equals("/v1/sub/5/3", "5 - 3 = 2").await;
     check_equals("/v1/sub/8/7", "8 - 7 = 1").await;
 }
 
 #[tokio::test]
-async fn test_multiply_v1() {
+async fn test_v1_multiply() {
     check_equals("/v1/mul/2/3", "2 * 3 = 6").await;
     check_equals("/v1/mul/7/8", "7 * 8 = 56").await;
 }
 
 #[tokio::test]
-async fn test_divide_v1() {
+async fn test_v1_divide() {
     check_equals("/v1/div/6/3", "6 / 3 = 2").await;
     check_equals("/v1/div/120/10", "120 / 10 = 12").await;
 }
 
 #[tokio::test]
-async fn test_add_v2() {
+async fn test_v1_other() {
+    let uri = "/v1/other/6/3";
+    let response = create_router()
+        .oneshot(Request::builder().uri(uri).body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    // No content-type
+    assert!(response.headers().get("content-type").is_none());
+
+    let body = response.into_body();
+    let bytes = body.collect().await.unwrap().to_bytes();
+    let content = String::from_utf8(bytes.to_vec()).unwrap();
+    assert_eq!(content, "");
+}
+
+#[tokio::test]
+async fn test_v2_add() {
     check_equals("/v2/add/2/3", "2 + 3 = 5").await;
     check_equals("/v2/add/7/8", "7 + 8 = 15").await;
 }
 
 #[tokio::test]
-async fn test_subtraction_v2() {
+async fn test_v2_subtraction() {
     check_equals("/v2/sub/5/3", "5 - 3 = 2").await;
     check_equals("/v2/sub/8/7", "8 - 7 = 1").await;
 }
 
 #[tokio::test]
-async fn test_multiply_v2() {
+async fn test_v2_multiply() {
     check_equals("/v2/mul/2/3", "2 * 3 = 6").await;
     check_equals("/v2/mul/7/8", "7 * 8 = 56").await;
 }
 
 #[tokio::test]
-async fn test_divide_v2() {
+async fn test_v2_divide() {
     check_equals("/v2/div/6/3", "6 / 3 = 2").await;
     check_equals("/v2/div/120/10", "120 / 10 = 12").await;
 }
+
+//#[tokio::test]
+//async fn test_v2_other() {
+//    let uri = "/v2/other/6/3";
+//    let response = create_router()
+//        .oneshot(Request::builder().uri(uri).body(Body::empty()).unwrap())
+//        .await
+//        .unwrap();
+//
+//    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+//
+//    // No content-type
+//    assert!(response.headers().get("content-type").is_none());
+//
+//    let body = response.into_body();
+//    let bytes = body.collect().await.unwrap().to_bytes();
+//    let content = String::from_utf8(bytes.to_vec()).unwrap();
+//    assert_eq!(content, "");
+//}
+//
 
 async fn check_contains(uri: &str, expected: &str) {
     let html = get_page(uri).await;
@@ -73,6 +112,10 @@ async fn get_page(uri: &str) -> String {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::OK);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type.to_str().unwrap(), "text/html; charset=utf-8");
+
     let body = response.into_body();
     let bytes = body.collect().await.unwrap().to_bytes();
     String::from_utf8(bytes.to_vec()).unwrap()
--- src/examples/versioning/src/tests.rs
+++ src/examples/versioning/src/tests.rs
@@ -17,6 +17,10 @@ async fn test_v1() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::OK);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type.to_str().unwrap(), "text/html; charset=utf-8");
+
     let body = response.into_body();
     let bytes = body.collect().await.unwrap().to_bytes();
     let html = String::from_utf8(bytes.to_vec()).unwrap();
@@ -37,9 +41,32 @@ async fn test_v4() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type.to_str().unwrap(), "text/plain; charset=utf-8");
+
     let body = response.into_body();
     let bytes = body.collect().await.unwrap().to_bytes();
     let html = String::from_utf8(bytes.to_vec()).unwrap();
 
     assert_eq!(html, "unknown version");
+}
+
+#[tokio::test]
+async fn test_api() {
+    let response = create_router()
+        .oneshot(Request::builder().uri("/api/").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    // no content-type
+    assert!(response.headers().get("content-type").is_none());
+
+    let body = response.into_body();
+    let bytes = body.collect().await.unwrap().to_bytes();
+    let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+    assert_eq!(html, "");
 }
