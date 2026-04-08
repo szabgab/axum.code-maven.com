983dbe127d2e34034b7dd0d424246ed82ef33c1e
Gabor Szabo <gabor@szabgab.com>
2026-04-08 11:38:39 +0200

    start working on todo list

---
+++ src/examples/todo/src/main.rs
@@ -0,0 +1,74 @@
+use askama::Template;
+use axum::{
+    Router,
+    extract::Query,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+};
+use serde::Deserialize;
+
+struct Item {
+    id: String,
+    title: String,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {err}"),
+            )
+                .into_response(),
+        }
+    }
+}
+
+#[derive(Template)]
+#[template(path = "main.html")]
+struct MainTemplate { title: String }
+
+// #[derive(Template)]
+// #[template(path = "edit.html")]
+// struct EditItemTemplate {
+// }
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct Params {
+    text: String,
+}
+
+async fn main_page() -> impl IntoResponse {
+    let template = MainTemplate { title: String::from("TODO") };
+    HtmlTemplate(template)
+}
+
+// async fn edit(Query(params): Query<Params>) -> impl IntoResponse {
+//     let template = EditTemplate {  };
+//     HtmlTemplate(template)
+// }
+
+fn create_router() -> Router {
+    Router::new()
+        .route("/", get(main_page))
+    //        .route("/edit", get(edit))
+}
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, create_router()).await.unwrap();
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/examples/todo/src/tests.rs
@@ -0,0 +1,94 @@
+use super::*;
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use http_body_util::BodyExt;
+use tower::ServiceExt;
+
+#[tokio::test]
+async fn test_main() {
+    let response = create_router()
+        .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let content_type = response.headers().get("content-type").unwrap();
+    assert_eq!(content_type.to_str().unwrap(), "text/html; charset=utf-8");
+
+    let body = response.into_body();
+    let bytes = body.collect().await.unwrap().to_bytes();
+    let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+    assert!(html.contains(r#"<title>TODO</title>"#));
+    //assert!(html.contains(r#"<form method="get" action="/echo">"#));
+}
+
+// #[tokio::test]
+// async fn test_echo_with_text() {
+//     let response = create_router()
+//         .oneshot(
+//             Request::builder()
+//                 .uri("/echo?text=Hello%20World")
+//                 .body(Body::empty())
+//                 .unwrap(),
+//         )
+//         .await
+//         .unwrap();
+//     assert_eq!(response.status(), StatusCode::OK);
+
+//     let content_type = response.headers().get("content-type").unwrap();
+//     assert_eq!(content_type.to_str().unwrap(), "text/html; charset=utf-8");
+
+//     let body = response.into_body();
+//     let bytes = body.collect().await.unwrap().to_bytes();
+//     let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+//     assert!(html.contains("You wrote: <b>Hello World</b>"));
+// }
+
+// #[tokio::test]
+// async fn test_echo_with_empty_text() {
+//     let response = create_router()
+//         .oneshot(
+//             Request::builder()
+//                 .uri("/echo?text=")
+//                 .body(Body::empty())
+//                 .unwrap(),
+//         )
+//         .await
+//         .unwrap();
+//     assert_eq!(response.status(), StatusCode::OK);
+
+//     let content_type = response.headers().get("content-type").unwrap();
+//     assert_eq!(content_type.to_str().unwrap(), "text/html; charset=utf-8");
+
+//     let body = response.into_body();
+//     let bytes = body.collect().await.unwrap().to_bytes();
+//     let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+//     assert!(html.contains("You did not write anything."));
+//     assert!(!html.contains("You wrote: <b>Hello World</b>"));
+// }
+
+// #[tokio::test]
+// async fn test_echo_without_text_param() {
+//     let response = create_router()
+//         .oneshot(Request::builder().uri("/echo").body(Body::empty()).unwrap())
+//         .await
+//         .unwrap();
+//     assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
+//     let content_type = response.headers().get("content-type").unwrap();
+//     assert_eq!(content_type.to_str().unwrap(), "text/plain; charset=utf-8");
+
+//     let body = response.into_body();
+//     let bytes = body.collect().await.unwrap().to_bytes();
+//     let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+//     assert_eq!(
+//         html,
+//         "Failed to deserialize query string: missing field `text`"
+//     );
+// }
